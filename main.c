6a888cc210dc69d705fee2434badf94c45bcb358
Mouhamad faruk <132808834+mouhamadfarukalshikhwiss@users.noreply.github.com>
2024-12-27 19:53:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -10,19 +10,6 @@
 #define MAX_CMD_LEN 1024 // Maksimum komut uzunluğu
 #define MAX_ARGS 64 // Maksimum argüman sayısı
 
-void prompt() {
-    printf("> "); // Kullanıcıdan komut istemi
-    fflush(stdout); // Çıkış akışını temizle
-}
-
-void handle_sigchld(int sig) {
-    int status;
-    pid_t pid;
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) { // Çocuk süreçlerin durumunu kontrol et
-        printf("[%d] retval: %d\n", pid, WEXITSTATUS(status)); // Çocuk sürecin çıkış durumunu yazdır
-        prompt(); // Komut istemini tekrar göster
-    }
-}
 
 void execute_single_command(char *cmd) {
     char *args[MAX_ARGS]; // Argümanları tutacak dizi
@@ -153,21 +140,6 @@ void execute_single_command(char *cmd) {
     }
 }
 
-void execute_command(char *cmd) {
-    char *commands[MAX_ARGS]; // Komutları tutacak dizi
-    char *token = strtok(cmd, ";"); // Komutları ';' ile ayır
-    int i = 0;
-
-    while (token != NULL) {
-        commands[i++] = token; // Komutu diziye ekle
-        token = strtok(NULL, ";");
-    }
-    commands[i] = NULL; // Komut dizisinin sonunu belirt
-
-    for (int j = 0; commands[j] != NULL; j++) {
-        execute_single_command(commands[j]); // Her bir komutu çalıştır
-    }
-}
 
 int main() {
     signal(SIGCHLD, handle_sigchld); // SIGCHLD sinyalini yakala
